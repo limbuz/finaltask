fae1c088ff9c6ae5c28efe2ca2dc364c78edbd87
limbuz <wwlimbuz@gmail.com>
2022-07-18 17:54:39 +0400

    added a Procfile

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=final',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=u1736884_yii2db',
+    'username' => 'u1736884_root',
+    'password' => 'uN6uV0xA1mmA8q',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
